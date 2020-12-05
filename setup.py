0424e8cc815f751bf7751f3443c6814a94ec4f8b
Aleš Daniel <ales.daniel@gmail.com>
2020-12-05 17:41:02 +0100

    Edit setup.py
    Remove comment from tutorial

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pelletics", # Replace with your own username
+    name="pelletics",
     version="0.0.1",
     author="Ales Daniel",
     author_email="ales.daniel@gmail.com",
