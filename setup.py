720790e3230f4d44505f6a5085910dd97cc7abd2
Aleš Daniel <ales.daniel@gmail.com>
2020-12-20 12:21:01 +0100

    Update setup.py
    Modified according to RealPython tutorial.

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
-import setuptools
+import pathlib
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-setuptools.setup(
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
     name="pelletics",
     version="0.0.1",
     author="Ales Daniel",
     author_email="ales.daniel@gmail.com",
     description="A pellet consumption statistics tool",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/adpro/pelletics",
-    packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
     python_requires='>=3.9',
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
 )
